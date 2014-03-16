5a7ab933a33e491d686898bc522b2f7de30cdddf
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-03-16 21:15:24 +0100

    Added test moving light in IntroScreen

--- src/system/screens/IntroScreen.hpp
+++ src/system/screens/IntroScreen.hpp
@@ -15,21 +15,38 @@ namespace sys
 
 class IntroScreen : public Screen
 {
-    sf::Texture tex;
-
 protected:
+    sf::CircleShape s;
+
     // Callbacks
-    virtual void    onUpdate() {}
+    virtual void    onUpdate()
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            s.setPosition(s.getPosition().x + 3.f, s.getPosition().y);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            s.setPosition(s.getPosition().x - 3.f, s.getPosition().y);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            s.setPosition(s.getPosition().x, s.getPosition().y - 3.f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            s.setPosition(s.getPosition().x, s.getPosition().y + 3.f);
+    }
+
     virtual void    onRender(gfx::Renderer & renderer) const
     {
         renderer.beginLight();
-        static sf::CircleShape s(256);
-        s.setFillColor(sf::Color::White);
         renderer.drawLight(s);
         renderer.endLight();
     }
 
-    virtual void    onShow() {}
+    virtual void    onShow()
+    {
+        s.setRadius(32.f);
+        s.setPosition(100.f, 100.f);
+        s.setFillColor(sf::Color::White);
+    }
     virtual void    onHide() {}
 
 };
