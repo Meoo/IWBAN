ebec6b131c32ade7468fdf67ff9d749c1d772bea
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-04-01 17:36:47 +0200

    Added ResourceError exception

---
+++ src/system/exceptions/ResourceError.hpp
@@ -0,0 +1,44 @@
+/**
+ * @file   ResourceError.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_RESOURCEERROR_HPP_
+#define _IWBAN_RESOURCEERROR_HPP_
+
+#include <Global.hpp>
+
+#include <system/Exception.hpp>
+
+namespace sys
+{
+
+class ResourceError : public Exception
+{
+private:
+    char _what[1024];
+
+
+public:
+    ResourceError(const char * what)
+    {
+        std::snprintf(_what, sizeof(_what), "%s", filename);
+    }
+
+    virtual const char * title() const noexcept
+    {
+        return "Resource error";
+    }
+
+    virtual const char * what() const noexcept
+    {
+        return _what;
+    }
+
+};
+// class ResourceError
+
+}
+// namespace sys
+
+#endif // _IWBAN_RESOURCEERROR_HPP_
