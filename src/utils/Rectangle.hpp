f7cef11337974224ab90cff406e6915e19d5b008
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-04-15 23:24:33 +0200

    Rectangle class

---
+++ src/utils/Rectangle.hpp
@@ -0,0 +1,59 @@
+/**
+ * @file   Rectangle.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_RECTANGLE_HPP_
+#define _IWBAN_RECTANGLE_HPP_
+
+namespace ut
+{
+
+class Rectangle
+{
+public:
+    float x;
+    float y;
+    float w;
+    float h;
+};
+// class Rectangle
+
+Rectangle intersect(const Rectangle & a, const Rectangle & b)
+{
+    Rectangle ret;
+
+    if (a.x > b.x)
+        ret.x = a.x;
+    else
+        ret.x = b.x;
+
+    if (a.y > b.y)
+        ret.y = a.y;
+    else
+        ret.y = b.y;
+
+    if (a.x + a.w < b.x + b.w)
+        ret.w = (a.x + a.w) - ret.x;
+    else
+        ret.w = (b.x + b.w) - ret.x;
+
+    if (a.y + a.h < b.y + b.h)
+        ret.h = (a.y + a.h) - ret.y;
+    else
+        ret.h = (b.y + b.h) - ret.y;
+
+    return ret;
+}
+
+bool hasIntersection(const Rectangle & a, const Rectangle & b)
+{
+    // TODO Can be optimized a little.
+    Rectangle i = intersect(a, b);
+    return i.w > 0 && i.h > 0;
+}
+
+}
+// namespace ut
+
+#endif // _IWBAN_RECTANGLE_HPP_
