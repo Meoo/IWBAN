ddebff50312023f0c9ca9d830abfd8f104086df4
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-04-23 17:49:24 +0200

    Improved test (prepared for moving platforms)

--- src/system/screens/IntroScreen.hpp
+++ src/system/screens/IntroScreen.hpp
@@ -22,7 +22,8 @@ class IntroScreen : public Screen
 {
 protected:
     phy::Simulator sim;
-    phy::Object * obj;
+    phy::Object * pl1;
+    phy::Object * pl2;
     phy::Object * obj2;
     phy::Object * obj3;
     phy::Object * obj4;
@@ -30,12 +31,14 @@ protected:
     phy::Box * box2;
     phy::PhysicsBehavior behavior;
 
+    unsigned ticks;
+
     // Callbacks
     virtual void    onUpdate()
     {
         const float spd = 3.f;
 
-        ut::Vector d = obj->getVelocity();
+        ut::Vector d = pl1->getVelocity();
         d.x = 0;
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
@@ -47,9 +50,37 @@ protected:
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             d.y = - 6;
 
-        obj->setVelocity(d);
+        pl1->setVelocity(d);
+
+        d = pl2->getVelocity();
+        d.x = 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+            d.x = spd;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::J))
+            d.x = - spd;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::I))
+            d.y = - 6;
+
+        pl2->setVelocity(d);
+
+        if (ticks < 100)
+        {
+            obj3->move(ut::Vector(1, 0));
+            obj4->move(ut::Vector(0, -1));
+        }
+        else
+        {
+            obj3->move(ut::Vector(-1, 0));
+            obj4->move(ut::Vector(0, 1));
+            if (ticks == 200) ticks = 0;
+        }
 
         sim.step();
+
+        ++ticks;
     }
 
     virtual void    onRender(gfx::Renderer & renderer) const
@@ -65,6 +96,8 @@ protected:
 
     virtual void    onShow()
     {
+        ticks = 0;
+
         ut::Rectangle rec;
         rec.x = -16;
         rec.y = -16;
@@ -80,9 +113,13 @@ protected:
         box = new phy::Box(rec);
         box2 = new phy::Box(rec2);
 
-        obj = new phy::Object(box, &behavior);
-        obj->setPosition(ut::Vector(200, 100));
-        obj->wake();
+        pl1 = new phy::Object(box, &behavior);
+        pl1->setPosition(ut::Vector(150, 100));
+        pl1->wake();
+
+        pl2 = new phy::Object(box, &behavior);
+        pl2->setPosition(ut::Vector(450, 100));
+        pl2->wake();
 
         obj2 = new phy::Object(box2);
         obj2->setPosition(ut::Vector(150, 250));
@@ -93,23 +130,32 @@ protected:
         obj4 = new phy::Object(box2);
         obj4->setPosition(ut::Vector(300, 400));
 
-        sim.add(*obj);
+        sim.add(*pl1);
+        sim.add(*pl2);
+
         sim.add(*obj2);
         sim.add(*obj3);
         sim.add(*obj4);
     }
+
     virtual void    onHide()
     {
-        sim.remove(*obj);
+        sim.remove(*pl1);
+        sim.remove(*pl2);
+
         sim.remove(*obj2);
         sim.remove(*obj3);
         sim.remove(*obj4);
 
         delete box;
         delete box2;
-        delete obj;
+
+        delete pl1;
+        delete pl2;
+
         delete obj2;
         delete obj3;
+        delete obj4;
     }
 
 };
