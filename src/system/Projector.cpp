e8415fc2ef871d458558a6d035e28d948fd81448
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-04-28 17:08:04 +0200

    Added the functions to change Screen from onUpdate()

--- src/system/Projector.cpp
+++ src/system/Projector.cpp
@@ -34,12 +34,25 @@ void Projector::setScreen(Screen * screen)
     _current_screen->onHide();
     _current_screen = screen;
     _current_screen->onShow();
+    _current_screen->setNextScreen(0);
 }
 
 void Projector::update()
 {
-    // TODO Update if screen is changed?
     _current_screen->onUpdate();
+
+    Screen * next_screen = _current_screen->getNextScreen();
+    if (next_screen)
+    {
+        BOOST_ASSERT(next_screen != _current_screen);
+
+        Screen * current = _current_screen;
+
+        setScreen(next_screen);
+
+        if (current->isDisposable())
+            delete current;
+    }
 }
 
 void Projector::render(gfx::Renderer & renderer) const
--- src/system/Screen.hpp
+++ src/system/Screen.hpp
@@ -21,9 +21,17 @@ public:
     friend class Projector;
 
 
+private:
+    // Data members
+    Screen *    _next_screen;
+    bool        _disposable;
+
+
 public:
     // Constructor
-                    Screen() {}
+                    Screen()
+                        : _next_screen(0), _disposable(false)
+                    {}
 
     // Destructor
     virtual         ~Screen() {}
@@ -36,6 +44,33 @@ protected:
 
     virtual void    onShow() = 0;
     virtual void    onHide() = 0;
+
+    // Internal functions
+    /**
+     * Set next screen to be projected.
+     *
+     * This function must be used in onUpdate() function, and
+     * using it from any other callback will have no effect.
+     *
+     * The screen will be changed right after the function onUpdate() returns.
+     */
+    void    setNextScreen(Screen * next_screen)
+        { BOOST_ASSERT(next_screen); _next_screen = next_screen; }
+
+    /**
+     * Mark this Screen to be released when the screen changes.
+     *
+     * @pre This function must be used after a call to setNextScreen(),
+     *      or an assertion will be raised.
+     */
+    void    dispose()
+        { BOOST_ASSERT(_next_screen); _disposable = true; }
+
+
+private:
+    // Functions for Projector
+    Screen *    getNextScreen() const   { return _next_screen; }
+    bool        isDisposable() const    { return _disposable; }
 
 };
 // class Screen
