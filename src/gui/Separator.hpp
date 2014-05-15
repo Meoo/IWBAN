98190c4663f34e922cabe5cef084230ce46da78e
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-05-15 23:36:31 +0200

    Add gui::Separator class

---
+++ src/gui/Separator.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file   Separator.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_SEPARATOR_HPP_
+#define _IWBAN_SEPARATOR_HPP_
+
+#include <Global.hpp>
+
+#include <gui/Element.hpp>
+
+namespace gui
+{
+
+class Separator : public Element
+{
+private:
+    // Data members
+    ut::Vector          _position;
+    ut::Vector          _size;
+
+
+public:
+    // Constructor
+    Separator(const ut::Vector & size) : _size(size) {}
+
+    // Virtual destructor
+    virtual ~Separator() {}
+
+    // Functions
+    virtual void draw(gfx::DrawContext & context) const {}
+
+    virtual ut::Vector getSize() const      { return _size; }
+    void setSize(const ut::Vector & size)   { _size = size; }
+
+    virtual void setPosition(const ut::Vector & position) { _position = position; }
+
+};
+// class Separator
+
+}
+// namespace gui
+
+#endif // _IWBAN_SEPARATOR_HPP_
