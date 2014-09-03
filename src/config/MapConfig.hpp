b7773c12229caee105183cce59adaceef85f5143
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-09-04 00:08:04 +0200

    Add config file for maps

---
+++ src/config/MapConfig.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file   MapConfig.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_MAPCONFIG_HPP_
+#define _IWBAN_MAPCONFIG_HPP_
+
+
+// File extension
+#define IWBAN_MAP_EXTENSION   ".map"
+
+// Map magic number 'IwMp' in little endian
+#define IWBAN_MAP_MAGIC       0x704D7749
+
+#define IWBAN_MAP_VERSION     1
+
+
+#endif // _IWBAN_MAPCONFIG_HPP_
