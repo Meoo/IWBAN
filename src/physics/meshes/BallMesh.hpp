04e6e79d3b9c7be4c1e734ec415e7af68ef4382b
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-09-28 19:06:49 +0200

    Add BallMesh

---
+++ src/physics/meshes/BallMesh.hpp
@@ -0,0 +1,80 @@
+/**
+ * @file   BallMesh.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_BALLMESH_HPP_
+#define _IWBAN_BALLMESH_HPP_
+
+#include <Global.hpp>
+
+#include <physics/Mesh.hpp>
+
+#ifndef NDEBUG
+#  include <SFML/Graphics/CircleShape.hpp>
+#endif
+
+namespace phy
+{
+
+class BallMesh : public Mesh
+{
+private:
+    ut::Rectangle   _bounds;
+    float           _radius;
+
+#ifndef NDEBUG
+    sf::CircleShape _shape;
+#endif
+
+
+public:
+            BallMesh() : _radius(0)
+    {
+#ifndef NDEBUG
+        _shape.setPointCount(16);
+        _shape.setFillColor(sf::Color::Transparent);
+        _shape.setOutlineColor(sf::Color::White);
+        _shape.setOutlineThickness(-2);
+#endif
+    }
+
+            BallMesh(float radius) : BallMesh()
+    {
+        setRadius(radius);
+    }
+
+    void    setRadius(float radius)
+    {
+        _radius         = radius;
+        _bounds.left    = - radius;
+        _bounds.top     = - radius;
+        _bounds.right   = radius;
+        _bounds.bottom  = radius;
+
+#ifndef NDEBUG
+        _shape.setRadius(radius);
+        _shape.setOrigin(radius, radius);
+#endif
+    }
+
+    const ut::Rectangle & getBounds() const override
+    {
+        return _bounds;
+    }
+
+#ifndef NDEBUG
+    void drawDebug(gfx::DebugContext & debug,
+                   const sf::RenderStates & states) const override
+    {
+        debug.draw(_shape, states);
+    }
+#endif
+
+};
+// class BallMesh
+
+}
+// namespace phy
+
+#endif // _IWBAN_BALLMESH_HPP_
