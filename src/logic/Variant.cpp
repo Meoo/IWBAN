da5128e1afd6c45e0e988c0398d274349bc5164c
Bastien Brunnenstein <bastien@brunnenstein.fr>
2014-10-05 16:41:03 +0200

    Add Variant class

---
+++ src/logic/Variant.cpp
@@ -0,0 +1,168 @@
+/**
+ * @file   Variant.cpp
+ * @author Bastien Brunnenstein
+ */
+
+#include <Global.hpp>
+
+// TODO Remove that... must find another way to fix boost's bug
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+
+#include <logic/Lua.hpp>
+#include <logic/Variant.hpp>
+
+#include <boost/lexical_cast.hpp>
+
+namespace logic
+{
+
+Variant::Variant()
+    : _type(TYPE_NULL)
+{
+}
+
+Variant::Variant(Int int_value)
+    : _type(TYPE_INT), _int_value(int_value)
+{
+}
+
+Variant::Variant(Float float_value)
+    : _type(TYPE_FLOAT), _float_value(float_value)
+{
+}
+
+Variant::Variant(bool bool_value)
+    : _type(TYPE_BOOL), _bool_value(bool_value)
+{
+}
+
+Variant::Variant(const Vector & vector_value)
+    : _type(TYPE_VECTOR), _vector_value(vector_value)
+{
+}
+
+Variant::Variant(const String & string_value)
+    : _type(TYPE_STRING), _string_value(string_value)
+{
+}
+
+// ---- ---- ---- ----
+
+Variant::Int Variant::toInt() const
+{
+    try
+    {
+        switch (_type)
+        {
+        case TYPE_INT:
+            return _int_value;
+
+        case TYPE_FLOAT:
+            return static_cast<Int>(_float_value);
+
+        case TYPE_BOOL:
+            return static_cast<Int>(_bool_value);
+
+        case TYPE_STRING:
+            return boost::lexical_cast<Int>(_string_value);
+
+        default: break;
+        }
+    }
+    catch(const boost::bad_lexical_cast &) {}
+
+    return 0;
+}
+
+Variant::Float Variant::toFloat() const
+{
+    try
+    {
+        switch (_type)
+        {
+        case TYPE_INT:
+            return static_cast<Float>(_int_value);
+
+        case TYPE_FLOAT:
+            return _float_value;
+
+        case TYPE_BOOL:
+            return static_cast<Float>(_bool_value);
+
+        case TYPE_STRING:
+            return boost::lexical_cast<Float>(_string_value);
+
+        default: break;
+        }
+    }
+    catch(const boost::bad_lexical_cast &) {}
+
+    return 0;
+}
+
+bool Variant::toBool() const
+{
+    try
+    {
+        switch (_type)
+        {
+        case TYPE_INT:
+            return static_cast<Float>(_int_value);
+
+        case TYPE_FLOAT:
+            return _float_value;
+
+        case TYPE_BOOL:
+            return static_cast<Float>(_bool_value);
+
+        case TYPE_STRING:
+            return boost::lexical_cast<bool>(_string_value);
+
+        default: break;
+        }
+    }
+    catch(const boost::bad_lexical_cast &) {}
+
+    return false;
+}
+
+Variant::Vector Variant::toVector() const
+{
+    switch (_type)
+    {
+    case TYPE_VECTOR:
+        return _vector_value;
+
+    default:
+        return Vector();
+    }
+}
+
+Variant::String Variant::toString() const
+{
+    try
+    {
+        switch (_type)
+        {
+        case TYPE_INT:
+            return boost::lexical_cast<String>(_int_value);
+
+        case TYPE_FLOAT:
+            return boost::lexical_cast<String>(_float_value);
+
+        case TYPE_BOOL:
+            return boost::lexical_cast<String>(_bool_value);
+
+        case TYPE_STRING:
+            return _string_value;
+
+        default: break;
+        }
+    }
+    catch(const boost::bad_lexical_cast &) {}
+
+    return String();
+}
+
+}
+// namespace logic
---
+++ src/logic/Variant.hpp
@@ -0,0 +1,86 @@
+/**
+ * @file   Variant.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _IWBAN_VARIANT_HPP_
+#define _IWBAN_VARIANT_HPP_
+
+#include <Global.hpp>
+
+#include <utils/Vector.hpp>
+
+#include <string>
+
+namespace logic
+{
+
+class Lua;
+
+class Variant
+{
+public:
+    typedef int         Int;
+    typedef float       Float;
+    typedef ut::Vector  Vector;
+    typedef std::string String;
+
+
+private:
+    // Type
+    enum Type
+    {
+        TYPE_NULL,
+        TYPE_INT,
+        TYPE_FLOAT,
+        TYPE_BOOL,
+        TYPE_VECTOR,
+        TYPE_STRING,
+
+    }           _type;
+    // enum Type
+
+    // Primitive types and POD
+    union
+    {
+        Int     _int_value;
+        Float   _float_value;
+        bool    _bool_value;
+        Vector  _vector_value;
+    };
+
+    // Complex classes
+    String      _string_value;
+
+
+public:
+    // Constructors
+                Variant();
+                Variant(Int int_value);
+                Variant(Float float_value);
+                Variant(bool bool_value);
+                Variant(const Vector & vector_value);
+                Variant(const String & string_value);
+
+    // Type check functions
+    bool        isNull() const      { return _type == TYPE_NULL; }
+    bool        isInt() const       { return _type == TYPE_INT; }
+    bool        isFloat() const     { return _type == TYPE_FLOAT; }
+    bool        isBool() const      { return _type == TYPE_BOOL; }
+    bool        isVector() const    { return _type == TYPE_VECTOR; }
+    bool        isString() const    { return _type == TYPE_STRING; }
+
+    // Conversion functions
+    Int         toInt() const;
+    Float       toFloat() const;
+    bool        toBool() const;
+    Vector      toVector() const;
+    String      toString() const;
+
+};
+// class Entity
+
+}
+// namespace logic
+
+#endif // _IWBAN_VARIANT_HPP_
